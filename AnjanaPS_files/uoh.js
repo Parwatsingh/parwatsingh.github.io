3767b2a46213138599a112cf9d04e384e89f88cb
Parwat Singh Anjana <45463871+Parwatsingh@users.noreply.github.com>
2020-06-15 16:29:49 +0530

    Updated uoh.js

--- AnjanaPS_files/uoh.js
+++ AnjanaPS_files/uoh.js
@@ -1,11 +1,11 @@
-var uoh = {} ;
+var Uoh = {} ;
 
 
-uoh.emailme = function (text) {
+Uoh.emailme = function (text) {
   if (!text) text = 'anjana.uoh' + '@gmail' + '.com.';
   document.write('<a href="mai'+'lto:' + 'anjana.uoh' + '@gmail' + '.com.' +text+'</a>');
 } ;
 
-uoh.email = function (account,domain,text) {
+Uoh.email = function (account,domain,text) {
   document.write('<a href="mailto:'+account+'@'+domain+'">'+text+'</a>') ;
 } ;
